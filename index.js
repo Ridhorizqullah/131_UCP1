85271544ac4ca6bc73cc34f20493fefa2d61c184
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-11-05 10:06:46 +0700

    menambahkan requirenya dan membuat app listen di index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const db = require('./models');
+const hotel = require('./models/hotel');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port,() => {
+    console.log(`Server is started on port`);
+})
